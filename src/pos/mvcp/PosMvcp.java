d594bc736c57db6b56f32c40797d668515d234f9
Prasadikengalle <prasadikengalle@gamil.com>
2023-09-02 23:40:27 +0530

    first commit

---
+++ src/pos/mvcp/PosMvcp.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pos.mvcp;
+
+/**
+ *
+ * @author Prasadi
+ */
+public class PosMvcp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
